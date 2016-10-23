b031a6d1593197e181f6ce3521e19c8f7a2d7ffd
Lucas Alves <lucas_comp@outlook.com>
2016-10-23 15:45:38 -0200

    Melhorando visualização

--- trabalhos/trabalho-03-04-05-06/main.lua
+++ trabalhos/trabalho-03-04-05-06/main.lua
@@ -1,56 +1,54 @@
 function love.load()
 	
-	--Declarando o mundo do jogo (sem gravidade)
 	love.physics.setMeter(64)
 	world = love.physics.newWorld(0, 0, true)
 	world:setCallbacks(beginContact)
 	
-	score = 0 --Pontuação atual
+	score = 0
+	-- Trabalho 04
 	-- Nome: score
 	-- Propriedade: Endereço
 	-- Binding Time: Compilação
 	-- Explicação: Por ser uma variável global, seu endereço é conhecido em tempo de compilação
-	
-	record = 0 --Maior pontuação alcançada
-	
-	temp = 0 --Contador de tempo para surgimento de bolas
 
-	aux = 0 --Auxiliar de intervalo entre tempo de surgimento de bolas
+	record = 0
+	
+	temp = 0
 
-	contball = 0 --Contador de bolas em movimento
+	aux = 0
+
+	contball = 0
 
 	gameover = false
 
-	objects = {} --Coleção dinamica de objetos (Trabalho 05: adicionar ao jogo uma coleção dinâmica de objetos)
-	--Escopo: Global
-	--Tempo de vida: Todo o tempo de execução
-	--Alocação: A alocação é feita dinamicamente e não tem tamanho fixo, ou seja, é ilimitada
-	--Desalocação: Ao final da execução
+	objects = {}
+	-- Trabalho 05: coleção dinâmica de objetos
+	-- Coleção dinamica de objetos
+	-- Escopo: Global
+	-- Tempo de vida: Todo o tempo de execução
+	-- Alocação: A alocação é feita dinamicamente e não tem tamanho fixo, ou seja, é ilimitada
+	-- Desalocação: Ao final da execução
 
-	bolas = {} --Coleção dinamica de objetos
+	bolas = {}
 	
-	--Criando a parede da esquerda
 	objects.wallLeft = {}
 	objects.wallLeft.body = love.physics.newBody(world, 5, love.graphics.getHeight()/2)
 	objects.wallLeft.shape = love.physics.newRectangleShape(10, love.graphics.getHeight())
 	objects.wallLeft.fixture = love.physics.newFixture(objects.wallLeft.body, objects.wallLeft.shape)
 	objects.wallLeft.fixture:setFriction(0)
 	
-	--Criando a parede da direita
 	objects.wallRight = {}
 	objects.wallRight.body = love.physics.newBody(world, love.graphics.getWidth()-5, love.graphics.getHeight()/2)
 	objects.wallRight.shape = love.physics.newRectangleShape(10, love.graphics.getHeight())
 	objects.wallRight.fixture = love.physics.newFixture(objects.wallRight.body, objects.wallRight.shape)
 	objects.wallRight.fixture:setFriction(0)
 	
-	--Criando o teto
 	objects.roof = {}
 	objects.roof.body = love.physics.newBody(world, love.graphics.getWidth()/2, 5)
 	objects.roof.shape = love.physics.newRectangleShape(love.graphics.getWidth(), 10)
 	objects.roof.fixture = love.physics.newFixture(objects.roof.body, objects.roof.shape)
 	objects.roof.fixture:setFriction(0)
 
-	--Criando a plataforma no chão
 	objects.polygon = {}
 	objects.polygon.body = love.physics.newBody(world, love.graphics.getWidth()/2, love.graphics.getHeight()-7.5)
 	objects.polygon.shape = love.physics.newPolygonShape(-75, -4, 1, -15, 75, -4, -75, 7.5, 75, 7.5)
@@ -61,6 +59,7 @@ function love.load()
 end
 
 function love.update(dt)
+	-- Trabalho 04
 	-- Nome: dt
 	-- Propriedade: Endereço
 	-- Binding time: Execução
@@ -70,6 +69,7 @@ function love.update(dt)
 	world:update(dt)
 	
 	temp = temp + dt
+	-- Trabalho 04
 	-- Nome: +
 	-- Propriedade: Semântica da linguagem
 	-- Binding time: Compilação
@@ -83,12 +83,12 @@ function love.update(dt)
 			j = j + 1
 		end
 
-		--Criando as bolas (Trabalho 05: criar novos objetos periodicamente)
+		-- Trabalho 05: criar novos objetos periodicamente
 		bolas[j] = {}
-		--Escopo: Global
-		--Tempo de vida: Desde a sua criação até a execução da linha 124
-		--Alocação: A alocação é feita dinamicamente. Neste caso, alocamos um array dentro de outro array
-		--Desalocação: Linha 124
+		-- Escopo: Global
+		-- Tempo de vida: Desde a sua criação até a execução da linha 124
+		-- Alocação: A alocação é feita dinamicamente. Neste caso, alocamos um array dentro de outro array
+		-- Desalocação: Linha 124
 
 		bolas[j].body = love.physics.newBody(world, love.graphics.getWidth()/2, 20, "dynamic")
 		bolas[j].shape = love.physics.newCircleShape(10)
@@ -99,37 +99,37 @@ function love.update(dt)
 		bolas[j].fixture:setUserData("Ball$contball")
 		contball = contball + 1
 	end
+	-- Trabalho 04
 	-- Nome: then
 	-- Propriedade: Semântica
 	-- Binding time: Design
 	-- Explicação: Foi definido, durante a implementação da linguagem, 
 	-- 	       que a palavra reservada 'then' definiria o fim da sentença lógica do bloco de decisão
 
-	--Movendo a plataforma
 	if love.keyboard.isDown("right") and not gameover then
 		objects.polygon.body:setPosition(objects.polygon.body:getX() + 18, love.graphics.getHeight()-7.5)
 	elseif love.keyboard.isDown("left") and not gameover then
 		objects.polygon.body:setPosition(objects.polygon.body:getX() - 18, love.graphics.getHeight()-7.5)
 	end
 	
-	--Limitando a plataforma até as paredes
 	if objects.polygon.body:getX() < 86 then
 		objects.polygon.body:setX(86)
 	elseif objects.polygon.body:getX() > love.graphics.getWidth()-86 then
 		objects.polygon.body:setX(love.graphics.getWidth()-86)
 	end
 
-	--Remover bolas (Trabalho 05: remover objetos)
+	-- Trabalho 05: remover objetos
 	if not gameover then
 		for i = 0, contball do
 			if bolas[i] ~= nil then
 				if bolas[i].body:getY() > love.graphics.getHeight() then
 					contball = contball - 1
-					bolas[i] = nil --Desaloca
+					bolas[i] = nil
 				end
 			end
 		end
 	end
+	-- Trabalho 04
 	-- Nome: end
 	-- Propriedade: Semântica
 	-- Binding time: Design
@@ -145,7 +145,6 @@ function love.update(dt)
 		objects.polygon.body:setPosition(love.graphics.getWidth()/2, love.graphics.getHeight()-7.5)
 	end
 
-	--Resetando o jogo
 	if love.keyboard.isDown("r") and gameover then
 		gameover = false
 		contball = 0
@@ -157,11 +156,9 @@ end
 
 function love.draw()
 
-	--Desenhando a plataforma
 	love.graphics.setColor(215, 60, 130)
 	love.graphics.polygon("fill", objects.polygon.body:getWorldPoints(objects.polygon.shape:getPoints()))
 	
-	--Desenhando as Bolas
 	for i = 0, contball do
 		if bolas[i] ~= nil then
 			love.graphics.setColor(255, 255, 255)
@@ -169,13 +166,11 @@ function love.draw()
 		end
 	end
 
-	--Desenhando o teto e as paredes
 	love.graphics.setColor(102, 204, 0)
 	love.graphics.polygon("fill", objects.roof.body:getWorldPoints(objects.roof.shape:getPoints()))
 	love.graphics.polygon("fill", objects.wallLeft.body:getWorldPoints(objects.wallLeft.shape:getPoints()))
 	love.graphics.polygon("fill", objects.wallRight.body:getWorldPoints(objects.wallRight.shape:getPoints()))
-	
-	--Desenhando a pontuação
+
 	if not gameover then
 		love.graphics.setColor(180, 180, 180)
 		love.graphics.print("Score: " .. score, 340, 280, 0, 2, 2)
@@ -191,8 +186,9 @@ function love.draw()
 	
 end
 
-
-function beginContact(a, b) --Colisão (Trabalho 05: Objetos devem interagir entre si)
+-- Trabalho 05: Objetos devem interagir entre si
+function beginContact(a, b)
+-- Trabalho 04
 -- Nome: b
 -- Propriedade: Valor
 -- Binding time: Execução
@@ -202,7 +198,6 @@ function beginContact(a, b) --Colisão (Trabalho 05: Objetos devem interagir ent
 	ator1 = a:getUserData()
 	ator2 = b:getUserData()
 
-	--Contando os contatos entre a Bola e a Plataforma
 	if ator1 ~= nil and ator2 ~= nil then
 		if	(a:getUserData() == "Floor" and (string.find(ator2, "Ball") ~= nil))  or ((string.find(ator1, "Ball") ~= nil) and b:getUserData()=="Floor") then
 			score = score + 1
